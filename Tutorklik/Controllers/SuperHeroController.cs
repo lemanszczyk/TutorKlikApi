2b3b1acd8078abde3af80c6b14f33b4c90978e8a
lemanszczyk <lemanczyk98@gmail.com>
2023-06-03 16:11:47 +0200

    Dodaj pliki projektów.

---
+++ Tutorklik/Controllers/SuperHeroController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.ExceptionServices;
+using Tutorklik.Data;
+
+namespace Hero_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly DataContext _context;  
+        public SuperHeroController(DataContext context)
+        {
+                _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHero()
+        {
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+    }
+}
---
+++ Tutorklik/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Hero_Api;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tutorklik.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options) { }
+        public DbSet<SuperHero> SuperHeroes => Set<SuperHero>();
+    }
+}
