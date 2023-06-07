01e3109ca9115adadde025c04b9c4a175e629590
lemanszczyk <lemanczyk98@gmail.com>
2023-06-07 16:40:22 +0200

    Controller add add, delete , update

--- Tutorklik/Controllers/SuperHeroController.cs
+++ Tutorklik/Controllers/SuperHeroController.cs
@@ -21,5 +21,36 @@ namespace Hero_Api.Controllers
         {
             return Ok(await _context.SuperHeroes.ToListAsync());
         }
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> CreateSuperHero(SuperHero hero)
+        {
+            _context.SuperHeroes.Add(hero);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateSuperHero(SuperHero hero)
+        {
+            var dbHero = await _context.SuperHeroes.FindAsync(hero.Id);
+            if (dbHero == null)
+            {
+                return BadRequest("Hero not found");
+            }
+            dbHero.Name = hero.Name;
+            await _context.SaveChangesAsync();
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> DeleteSuperHero(int id)
+        {
+            var dbHero = await _context.SuperHeroes.FindAsync(id);
+            if (dbHero == null)
+            {
+                return BadRequest("Hero not found");
+            }
+            _context.SuperHeroes.Remove(dbHero);
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
     }
 }
